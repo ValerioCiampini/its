532b7d423e73c90abe3b56e862c17c2fe6857945
ValerioCiampini <valeriociamp2004@gmail.com>
2025-04-08 19:25:18 +0200

    es_2

--- Python/Lezione_13/es_2_funzioni_ricorsive.py
+++ Python/Lezione_13/es_2_funzioni_ricorsive.py
@@ -2,8 +2,8 @@ def compoundInterest(m:int, t:int) -> int:
     if t == 0:
         return 0
     else:
-        return m + (m * 1005) + compoundInterest(m * 1005, t - 1)
+        return m + compoundInterest(m * 1005, t - 1)
     
 
-print(compoundInterest(1, 2))
+print(compoundInterest(1, 4))
         
