d8a828afcb0b4976a50cf39c1eda682a51692b95
ValerioCiampini <valeriociamp2004@gmail.com>
2025-07-23 10:57:55 +0200

    sql

---
+++ Database/ordini_fatture.sql
@@ -0,0 +1,100 @@
+create domain PosInteger as integer
+    check(value >= 0);
+
+create domain Stringa as varchar;
+
+create domain StringaM as varchar(100);
+
+create domain CAP as char(5);
+    check(value ~ '[0-9]{5}')
+
+create type Indirizzo as (
+    via Stringa, 
+    civico Stringa,
+    cap CAP 
+);
+
+create domain RealGEZ as real
+    check(value >= 0);
+
+create domain RealBZO as real
+    check(value >= 0 and value <= 1);
+
+create domain CodiceFiscale as char(16);
+
+create domain Telefono as char(20)
+ check(value '[0-9]*10');
+
+create domain Email as char(100)
+    check(value ~ '%^[A-Za-z0-9._%\-+!#$&/=?^|~]+@[A-Za-z0-9.-]+[.][A-Za-z]+$%');
+
+create domain Partita_iva as char(11)
+    check(value ~ '^[0-9]{11}');
+
+create type StatoOrdine as 
+    enum('in preparazione', 'inviato', 'da saldare', 'saldato');
+
+create table Direttore(
+    nome Stringa not null,
+    cognome Stringa not null,
+    cf CodiceFiscale not null,
+    anni_servizio PosInteger not null,
+    data_nascita date not null,
+
+    primary key(cf),
+    unique(cf)
+);
+
+create table Dipartimento(
+    nome Stringa not null,
+    indirizzo Indirizzo not null,
+
+    primary key(nome),
+    unique(nome)
+);
+
+create table Ordine(
+    data_stipula date not null,
+    imponibile RealGEZ not null,
+    aliquota RealBZO not null,
+    descrizione StringaM not null,
+    id integer not null,
+
+    primary key(id)
+);
+
+create table StatoOrdine(
+    nome Stringa not null,
+
+    primary key(nome)
+);
+
+create table Fornitore(
+    ragione_sociale Stringa not null,
+    partita_iva Partita_iva not null,
+    indirizzo Indirizzo not null,
+    telefono Telefono not null,
+    email Email not null,
+
+    primary key(partita_iva),
+    unique(partita_iva)
+);
+
+create table Citta(
+    nome Stringa not null,
+
+    primary key(nome)
+);
+
+create table Regione(
+    nome Stringa not null,
+
+    primary key(nome)
+);
+
+create table Nazione(
+    nome Stringa not null,
+
+    primary key(nome)
+);
+
