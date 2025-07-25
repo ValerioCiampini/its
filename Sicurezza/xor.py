6c79eafbbec3fb076db3a9297bc4646ba57b19a9
ValerioCiampini <valeriociamp2004@gmail.com>
2025-07-25 13:42:58 +0200

    sicurezza

---
+++ Sicurezza/xor.py
@@ -0,0 +1,17 @@
+frase = "Nel mezzo del cammin di nostra vita"
+valore = 57
+lista1 = []
+lista2 = []
+lista3 = []
+
+for i in range(len(frase)):
+    lista1.append(ord(frase[i]))
+    lista2.append(lista1[i] ^ valore)
+
+print(lista2)
+
+
+for i in lista2:
+    lista3.append(chr(i ^ 57))
+
+print(lista3)
